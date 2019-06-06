a5b3167b3d340cdac9e3349d7b51b3dacb1db143
bhuvnesh.kumar <bhuvnesh.kumar@TeamCity>
2019-06-06 12:13:21 -0700

    TeamCity change in 'AppDynamics Extensions / Cassandra Monitoring Extension' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/AE_CassandraMonitoringExtension/vcsRoots/cassandramonitoringextension.kt
@@ -0,0 +1,15 @@
+package AE_CassandraMonitoringExtension.vcsRoots
+
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
+
+object cassandramonitoringextension : GitVcsRoot({
+    uuid = "de423cab-7e1b-4ba5-8727-b321715da94d"
+    name = "cassandramonitoringextension"
+    url = "ssh://git@bitbucket.corp.appdynamics.com:7999/ae/cassandra-monitoring-extension.git"
+    pushUrl = "ssh://git@bitbucket.corp.appdynamics.com:7999/ae/cassandra-monitoring-extension.git"
+    branch = "refs/heads/2.1"
+    authMethod = uploadedKey {
+        uploadedKey = "TeamCity BitBucket Key"
+    }
+})
